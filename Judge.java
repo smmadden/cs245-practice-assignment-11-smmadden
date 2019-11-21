a4f5bec2b2647c0c06758a37ec09e35ad3f09b90
smmadden <smmadden10@gmail.com>
2019-11-21 13:42:21 -0800

    commit

--- Judge.java
+++ Judge.java
@@ -1,5 +1,19 @@
-public class Judge{
+public class Judge
+{
+  // possibly some algorithm?
+  // probably don't need an algorithm
+  // directed graph - find the one with no edges out? Is that easy to do quickly?
+  // You are expected to design a solution and implement the design.
+  // could list everything in first column and everything in the second and find one that is not in the first column and not in the second and if nothing like that exists then return -1
+  // just like, a loop situation
+  // runtime = shit probably
+  // but thats ok i hope
+  // because there's nothing about runtime on the assignment sheet so have at it who cares about elegance
+
+
   public static void main(String[] args){
     System.out.println("Initial commit");
   }
+
 }
+
